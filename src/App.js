96160a80d55f0b0370102d5637e26ba649d3ac6c
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2022-04-21 11:25:12 -0700

    changes made to app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello world
+          git commands using vs code
         </a>
       </header>
     </div>
